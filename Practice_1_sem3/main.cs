fbfd17df7e763f0a07dd11f2412cc6f5acd9978e
Veteroch4k <indefinite05@mail.ru>
2024-09-30 03:22:52 +0700

    Добавьте файлы проекта.

---
+++ Practice_1_sem3/main.cs
@@ -0,0 +1,14 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+
+namespace Practice_1_sem3
+{
+    class Program
+    {
+        public static void Main()
+        {
+
+           ConsoleHelper.showInterface();
+
+        }
+    }
+}
